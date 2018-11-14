7bded3963928125668b1b4ebf7d00fc504ae465a
Evan Pavley <epavley@sva.edu>
2018-11-14 23:24:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,10 +2,10 @@ function forLoop() {
   const array = []
 
   for (let i = 1; i < 25; i++) {
-    if (i===1)
+    if (i===1){
     array.push("I am a strange loop.")
   }
-
+}
   return [array]
 }
 
